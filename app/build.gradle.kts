12ff6aa826517f3958929313bbeb7f1946d727a6
io681 <io-68@yandex.ru>
2023-12-21 23:12:45 +0300

    create Makefiles, update build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    id("checkstyle")
+    application
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+//application { mainClass.set("hexlet.code.App") }
+
+tasks.compileJava {
+    options.release = 20
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
