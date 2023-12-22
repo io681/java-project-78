f8324c2402a935337aa50fff30f1b147afa26047
io681 <io-68@yandex.ru>
2023-12-22 20:44:23 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
     id("checkstyle")
@@ -19,6 +22,17 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+
+    maxHeapSize = "1G"
+
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true    }
+
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 application { mainClass.set("hexlet.code.App") }
