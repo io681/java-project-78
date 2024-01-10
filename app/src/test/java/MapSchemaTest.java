0c542b5a199cccb0298360878240383769d7bd8c
io681 <io-68@yandex.ru>
2024-01-10 15:51:36 +0300

    add MapSchemaTest

---
+++ app/src/test/java/MapSchemaTest.java
@@ -0,0 +1,78 @@
+import hexlet.code.MapSchema;
+import hexlet.code.Validator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MapSchemaTest {
+    private MapSchema schema;
+
+    @BeforeEach
+    void init() {
+        Validator v = new Validator();
+        schema = v.map();
+    }
+
+    @Test
+    void testSimplyIsValid() {
+        boolean result1 = schema.isValid(null);
+        boolean result2  = schema.isValid(new HashMap<>());
+
+        Map<String, String> data = new HashMap<>();
+        data.put("key1", "value1");
+        boolean result3  = schema.isValid(data);
+
+        boolean result4 = schema.isValid(10);
+
+        assertTrue(result1, "Failed test");
+        assertTrue(result2, "Failed test");
+        assertTrue(result3, "Failed test");
+        assertFalse(result4, "Failed test");
+    }
+
+    @Test
+    void testRequired() {
+        schema.required();
+        boolean result1 = schema.isValid(null);
+        boolean result2  = schema.isValid(new HashMap<>());
+
+        Map<String, String> data = new HashMap<>();
+        data.put("key1", "value1");
+        boolean result3  = schema.isValid(data);
+
+        Map<String, String> data2 = new HashMap<>();
+        data2.put("key1", null);
+        boolean result4 = schema.isValid(data2);
+
+        assertFalse(result1, "Failed test");
+        assertTrue(result2, "Failed test");
+        assertTrue(result3, "Failed test");
+        assertFalse(result4, "Failed test");
+    }
+
+    @Test
+    void testSizeof() {
+        schema.sizeof(2);
+        boolean result1 = schema.isValid(null);
+        boolean result2  = schema.isValid(new HashMap<>());
+
+        Map<String, String> data = new HashMap<>();
+        data.put("key1", "value1");
+        boolean result3  = schema.isValid(data);
+
+        Map<String, String> data2 = new HashMap<>();
+        data2.put("key1", "value1");
+        data2.put("key2", "value2");
+        boolean result4 = schema.isValid(data2);
+
+        assertFalse(result1, "Failed test");
+        assertFalse(result2, "Failed test");
+        assertFalse(result3, "Failed test");
+        assertTrue(result4, "Failed test");
+    }
+}
