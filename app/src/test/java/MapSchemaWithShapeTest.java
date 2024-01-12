5886e3ca661a7c4309a5306d05e82cf39d4209c2
io681 <io-68@yandex.ru>
2024-01-12 12:51:03 +0300

    add test MapSchema with shape()

---
+++ app/src/test/java/MapSchemaWithShapeTest.java
@@ -0,0 +1,60 @@
+import hexlet.code.BaseSchema;
+import hexlet.code.MapSchema;
+import hexlet.code.Validator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MapSchemaWithShapeTest {
+    @BeforeAll
+    void init() {
+        Validator v = new Validator();
+        MapSchema schema = v.map();
+
+        Map<String, BaseSchema> schemas = new HashMap<>();
+        schemas.put("name", v.string().required());
+        schemas.put("age", v.number().positive());
+        schema.shape(schemas);
+    }
+
+    @Test
+    public void shapeTest1() {
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Kolya");
+        human1.put("age", 100);
+        boolean result = schema.isValid(human1);
+        assertTrue(result, "Failed test");
+    }
+
+    @Test
+    public void shapeTest2() {
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        boolean result = schema.isValid(human2);
+        assertTrue(result, "Failed test");
+    }
+
+    @Test
+    public void shapeTest3() {
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        boolean result = schema.isValid(human3);
+        assertFalse(result, "Failed test");
+    }
+
+    @Test
+    public void shapeTest4() {
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Valya");
+        human4.put("age", -5);
+        boolean result = schema.isValid(human4);
+        assertFalse(result, "Failed test");
+    }
+}
