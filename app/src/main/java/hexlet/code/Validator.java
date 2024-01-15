4878e29010120e63c7436126408ed09b9ed73688
io681 <io-68@yandex.ru>
2024-01-15 15:14:03 +0300

    deleted getSchema() in Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -6,28 +6,14 @@ import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
-    public static final String STRING_SCHEMA = "StringSchema";
-    public static final String NUMBER_SCHEMA = "NumberSchema";
-    public static final String MAP_SCHEMA = "MapSchema";
     BaseSchema schema;
     public StringSchema string() {
-        return (StringSchema) getSchema("StringSchema");
+        return new StringSchema();
     }
     public NumberSchema number() {
-        return (NumberSchema) getSchema("NumberSchema");
+        return new NumberSchema();
     }
     public MapSchema map() {
-        return (MapSchema) getSchema("MapSchema");
-    }
-
-    public BaseSchema getSchema(String name) {
-        if (name.equals(STRING_SCHEMA)) {
-            return new StringSchema();
-        } else if (name.equals(NUMBER_SCHEMA)) {
-            return new NumberSchema();
-        } else if (name.equals(MAP_SCHEMA)) {
-            return new MapSchema();
-        }
-        return null;
+        return new MapSchema();
     }
 }
